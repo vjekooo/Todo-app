05d46ceed742cafc8f697bcefc9c1c46196e7293
vjekooo <vjeko.pavicin@gmail.com>
2017-03-09 13:08:29 +0100

    Added an option to hide completed tasks

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -7,6 +7,15 @@ import Task from './Task.jsx';
 
 // App component - represents the whole app
 class App extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+          hideCompleted: false,
+        };
+    }
+
     handleSubmit(event) {
         event.preventDefault();
 
@@ -22,40 +31,61 @@ class App extends Component {
         ReactDOM.findDOMNode(this.refs.textInput).value = '';
     }
 
+    toggleHideCompleted() {
+        this.setState({
+          hideCompleted: !this.state.hideCompleted,
+        });
+    }
+
     renderTasks() {
-        return this.props.tasks.map((task) => (
-          <Task key={task._id} task={task} />
+        let filteredTasks = this.props.tasks;
+        if (this.state.hideCompleted) {
+            filteredTasks = filteredTasks.filter(task => !task.checked);
+        }
+        return filteredTasks.map((task) => (
+            <Task key={task._id} task={task} />
         ));
     }
 
-  render() {
-    return (
-      <div className="container">
-        <header>
-          <h1>Todo List</h1>
-          <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
-            <input
-              type="text"
-              ref="textInput"
-              placeholder="Type to add new tasks"
-            />
-          </form>
-        </header>
+    render() {
+        return (
+            <div className="container">
+            <header>
+            <h1>Todo List</h1>
 
-        <ul>
-          {this.renderTasks()}
-        </ul>
-      </div>
-    );
-  }
+            <label className="hide-completed">
+            <input
+                type="checkbox"
+                readOnly
+                checked={this.state.hideCompleted}
+                onClick={this.toggleHideCompleted.bind(this)}
+            />
+                Hide Completed Tasks
+            </label>
+
+            <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
+            <input
+                type="text"
+                ref="textInput"
+                placeholder="Type to add new tasks"
+            />
+            </form>
+            </header>
+
+            <ul>
+                {this.renderTasks()}
+            </ul>
+            </div>
+        );
+    }
 }
 
 App.propTypes = {
-  tasks: PropTypes.array.isRequired,
+    tasks: PropTypes.array.isRequired,
 };
 
 export default createContainer(() => {
-  return {
-    tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
-  };
+    return {
+        tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
+    };
 }, App);
