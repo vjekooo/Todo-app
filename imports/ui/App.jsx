f3a5e0364eda24f5269ef6fb90816567ef2303de
vjekooo <vjeko.pavicin@gmail.com>
2017-03-09 13:15:38 +0100

    Return number of incomplete tasks and diplay it in the header

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -51,7 +51,7 @@ class App extends Component {
         return (
             <div className="container">
             <header>
-            <h1>Todo List</h1>
+            <h1>Todo List ({this.props.incompleteCount})</h1>
 
             <label className="hide-completed">
             <input
@@ -82,10 +82,12 @@ class App extends Component {
 
 App.propTypes = {
     tasks: PropTypes.array.isRequired,
+    incompleteCount: PropTypes.number.isRequired,
 };
 
 export default createContainer(() => {
     return {
         tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
+        incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
     };
 }, App);
